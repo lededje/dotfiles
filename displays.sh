e6b0f7c3f6e5142f3c05ea435112ff2bb16ca9c5
Miles Spoor <git@likeminded.io>
2021-02-17 13:53:59 +0100

    Updated display layout

--- displays.sh
+++ displays.sh
@@ -1,6 +1,4 @@
-xrandr --output DVI-D-0 --mode 2560x1440 --rate 59.95
-xrandr --output HDMI-0 --mode 2560x1440 --rate 59.95
-xrandr --output DP-0 --mode 2560x1440 --rate 144.00
-xrandr --output HDMI-0 --right-of DP-0
-xrandr --output DVI-D-0 --left-of DP-0
+xrandr --output DVI-D-0 --mode 2560x1440 --rate 59.95 --pos 0x0
+xrandr --output DP-0 --mode 2560x1440 --rate 144.00 --pos 2560x0 --primary
+xrandr --output HDMI-0 --mode 2560x1440 --rate 59.95 --pos 5120x0
 
