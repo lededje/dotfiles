340b272611cac57c8a3739940292101db12b95bc
Miles Spoor <git@likeminded.io>
2021-02-17 11:31:49 +0100

    Updated aliases file name, added display script

---
+++ displays.sh
@@ -0,0 +1,6 @@
+xrandr --output DVI-D-0 --mode 2560x1440 --rate 59.95
+xrandr --output HDMI-0 --mode 2560x1440 --rate 59.95
+xrandr --output DP-0 --mode 2560x1440 --rate 144.00
+xrandr --output HDMI-0 --right-of DP-0
+xrandr --output DVI-D-0 --left-of DP-0
+
